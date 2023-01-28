28456b8112e51916b006a68f8ca74cb22b96b691
JanDaBaboon <junyaswift@gmail.com>
2023-01-28 22:37:36 +0900

    added comments to code for login

--- BuymaAutomation.py
+++ BuymaAutomation.py
@@ -2,27 +2,30 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
 
+#information for website and driver
 url = "https://www.buyma.com/login/"
 driver = webdriver.Chrome(executable_path="./chromedriver")
 
-
+#opens the website
 driver.get(url)
 
+#ids for different input elements
 login_id = "txtLoginId"
 password_id = "txtLoginPass"
 submit_element_id = "login_do"
 
+#Function for logging in. Used for both email and password.
 def Login(input, id):
     field = driver.find_element(By.ID, id)
     for x in input :
         field.send_keys(x)
     return driver
 
+#Clicking function that clicks on specificied ID
 def Click(id) :
     driver.find_element(By.ID, id).click()
     return driver
     
-
 
 Login("kokomimi.buyma@gmail.com", login_id)
 Login("koko12345", password_id)
