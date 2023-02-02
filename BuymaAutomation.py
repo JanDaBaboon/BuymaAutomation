e447c9267e52926383b9e498fd7b987bb84a0e35
JanDaBaboon <junyaswift@gmail.com>
2023-02-02 16:08:05 +0900

    added nav to item add menu, added skip of tutorial, added pause func in program

--- BuymaAutomation.py
+++ BuymaAutomation.py
@@ -1,18 +1,29 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
+import keyboard
+
+def pause():
+    while True:
+        if keyboard.read_key() == 'space':
+            break
+        
+        
+        
+        
+        
 
 #information for website and driver
 url = "https://www.buyma.com/login/"
+url2 = "https://www.buyma.com/my/sell/new?tab=b"
 driver = webdriver.Chrome(executable_path="./chromedriver")
 
-#opens the website
-driver.get(url)
-
-#ids for different input elements
+#selection elements in order of uses for different input elements
 login_id = "txtLoginId"
 password_id = "txtLoginPass"
 submit_element_id = "login_do"
+skip_button_class = "driver-close-btn"
+
 
 #Function for logging in. Used for both email and password.
 def Login(input, id):
@@ -22,12 +33,31 @@ def Login(input, id):
     return driver
 
 #Clicking function that clicks on specificied ID
-def Click(id) :
+def Click_ID(id) :
     driver.find_element(By.ID, id).click()
+    return driver
+
+def Click_Class(id) :
+    driver.find_element(By.CLASS_NAME, id).click()
     return driver
     
 
+driver.get(url)    
+
 Login("kokomimi.buyma@gmail.com", login_id)
+
 Login("koko12345", password_id)
-Click(submit_element_id)
+
+Click_ID(submit_element_id)
+
+driver.get(url2)
+
+Click_Class(skip_button_class)
+
+
+pause()
 time.sleep(5)
+
+
+
+
