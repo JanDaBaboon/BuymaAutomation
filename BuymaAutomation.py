6eb7a6ffea08b6c0bda02fe72034545cad813846
JanDaBaboon <junyaswift@gmail.com>
2023-02-02 19:10:26 +0900

    added new files for program. added functionality to read spreadsheet. added ability to upload photos to buyma

--- BuymaAutomation.py
+++ BuymaAutomation.py
@@ -2,30 +2,38 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
 import keyboard
+from openpyxl import workbook, load_workbook
 
+
+#-----------------------------------------------------------------------------
+#temp code
 def pause():
     while True:
         if keyboard.read_key() == 'space':
             break
         
+           
         
-        
-        
-        
-
-#information for website and driver
+#-----------------------------------------------------------------------------
+#information for website, driver, and xlsx
 url = "https://www.buyma.com/login/"
 url2 = "https://www.buyma.com/my/sell/new?tab=b"
 driver = webdriver.Chrome(executable_path="./chromedriver")
+wb = load_workbook(r"C:\Users\junya\Desktop\BuymaAutomation\xlsx\inventory_data.xlsx")
+ws = wb["inventory_data_sheet"]
+image_location = r"C:\Users\junya\Desktop\BuymaAutomation\images\Malta Gown\img(101).jpg"
 
 #selection elements in order of uses for different input elements
 login_id = "txtLoginId"
 password_id = "txtLoginPass"
 submit_element_id = "login_do"
 skip_button_class = "driver-close-btn"
+drag_image_xpath = "/html/body/div[3]/div[2]/div[1]/div/div[1]/div/div/div/div[2]/form/div[1]/div/div/div[2]/div/div/div[1]/div/div/div/input"
 
 
-#Function for logging in. Used for both email and password.
+#-----------------------------------------------------------------------------
+#Functions for navigation
+
 def Login(input, id):
     field = driver.find_element(By.ID, id)
     for x in input :
@@ -37,12 +45,27 @@ def Click_ID(id) :
     driver.find_element(By.ID, id).click()
     return driver
 
-def Click_Class(id) :
-    driver.find_element(By.CLASS_NAME, id).click()
+def Click_Class(class_id) :
+    driver.find_element(By.CLASS_NAME, class_id).click()
     return driver
     
 
+def Upload_Photo_xPath(xPath) :
+    driver.find_element(By.XPATH, xPath).send_keys(image_location)
+    return driver
+
+def AccessSheet(sheet) :
+    for col in sheet.iter_cols(min_row=2, max_col=1, max_row=10):
+        for cell in col:
+            if cell.value != None :
+                print (cell.value)
+            
+
+# -----------------------------------------------------------------------------
+# Order of operations
+
 driver.get(url)    
+driver.maximize_window()
 
 Login("kokomimi.buyma@gmail.com", login_id)
 
@@ -54,9 +77,15 @@ driver.get(url2)
 
 Click_Class(skip_button_class)
 
+AccessSheet(ws)
+
+Upload_Photo_xPath(drag_image_xpath)
+
+
+
 
 pause()
-time.sleep(5)
+time.sleep(1)
 
 
 
