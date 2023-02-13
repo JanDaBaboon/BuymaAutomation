3cd6a5543000b901777a4ce9dd8bf46ab0233c60
JanDaBaboon <junyaswift@gmail.com>
2023-02-13 17:38:55 +0900

    additional comments

--- BuymaAutomation.py
+++ BuymaAutomation.py
@@ -53,27 +53,28 @@ def Click_Class(class_id) :
     driver.find_element(By.CLASS_NAME, class_id).click()
     return driver
     
-#Function to upload photos
+#Function to upload photos. Iliterates through each image in given item's folder. Changes Xpath input according to current photo count. 
 def Upload_Photo_xPath(xPath) :
+    
     current_item_image_location = os.path.join(image_location, item_list[item_pointer])
-    #print(current_item_image_location)
+
     photo_counter = 1
+    
     for images in os.listdir(current_item_image_location):
+        
         i = os.path.join(current_item_image_location, images)
+        
         if photo_counter == 1:
             driver.find_element(By.XPATH, xPath).send_keys(i)
             photo_counter =+ 1
             time.sleep(2)
+            
         else :
             xPath2 = ("/html/body/div[3]/div[2]/div[1]/div/div[1]/div/div/div/div[2]/form/div[1]/div/div/div[2]/div/div/div[1]/div/div/div[%s]/input" % photo_counter)
             driver.find_element(By.XPATH, xPath2).send_keys(i)
             time.sleep(1)
             return driver
-          
-            
-    
         
-
 
 #Function to access xlsx sheet. Returns a list with all the items of first row.
 def AccessSheet(sheet) :
@@ -84,30 +85,9 @@ def AccessSheet(sheet) :
                 print(item_list)           
    
          
-            
 # -----------------------------------------------------------------------------
-#Tracking which item we are on
+#Tracking which item we are on. Change the counter to change item.
 item_pointer = 0
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 # -----------------------------------------------------------------------------
 # Order of operations
